97010ce7fbe5458f9de50a4ca2c0ccdc3bfb4b9a
ScottGersten <130173316+ScottGersten@users.noreply.github.com>
2024-12-18 16:21:21 -0500

    Add project files.

---
+++ DataStructuresApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataStructuresApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DataStructuresApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DataStructuresApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
